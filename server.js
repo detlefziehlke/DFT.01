77fd9b67f5ec83fd838130eedde7e3cc32b8b6fe
detti <detlefziehlke@googlemail.com>
2015-03-07 11:23:39 +0100

    once again (not all files were selected in the former commit)

--- server.js
+++ server.js
@@ -9,16 +9,23 @@ app.use(bodyParser.urlencoded({extended: true}));
 var port = process.env.port || 8080;
 var serverProject = "DFT.01";
 
-var routes = require('./routes');
+if (app.get('env') === 'test')
+  app.set('finance_db', './Databases/FinanzDb_Test.db3');
+else
+  app.set('finance_db', './Databases/FinanzDb_Prod.db3')
 
+var routes = require('./routes');
+routes.finance.setup(app)
+;
 app.get('/finance/api/getBalances', routes.finance.api.getBalances);
 
 app.use(function (err, req, res, nexgt) {
-  var msg ='invalid request / error occured: ' + err;
+  var msg = 'invalid request / error occured: ' + err;
   console.log(msg);
   res.send(msg);
 });
 
 app.listen(port);
-console.log('Server (' + serverProject + ') is running at port ' + port);
+console.log('database file for finance app used: ' + app.get('finance_db'));
+console.log('Server (' + serverProject + ') is running at port ' + port + ' in mode ' + app.get('env'));
 
