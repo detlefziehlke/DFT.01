504a713831a315ee10c9d86cbfaaa49780421e66
detti <detlefziehlke@googlemail.com>
2015-03-15 11:58:16 +0100

    getEntries (by AccountId or Name) implemented leftjoins in Empfänger und Kategorie coalesce, knex columns,  knex.raw
    still missing: resolving subcat form cat

--- routes/finance/api.js
+++ routes/finance/api.js
@@ -23,32 +23,35 @@ exports.api = {
     var acc = req.params.acc;
     var where;
     if (isNaN(acc))
-      where = {'Name': acc};
+      where = {'Konto.Name': acc};
     else
       where = {'Konto.Id': acc};
 
-    knex.from('Konto')
+    var columns = [
+      'Buchung.Id as buchung_id',
+      'Datum as datum',
+      'Betrag as betrag',
+      'Memo as memo',
+      'Konto.Id as konto_id',
+      'Konto.Name as konto_name',
+      knex.raw('coalesce(Empfaenger.Id,-1) as empfaenger_id'),
+      knex.raw('coalesce(Empfaenger.Name,"*** no emp ***") as empfaenger_name'),
+      knex.raw('coalesce(Kategorie.Id,-1) as kategorie_id'),
+      knex.raw('coalesce(Kategorie.Name,"*** no cat ***") as kategorie_name')
+    ];
+
+    var x = knex
+        .select(columns)
+        .table('Konto')
+
         .innerJoin('Buchung', 'Konto.Id', 'Buchung.KontoId')
+        .leftJoin('Empfaenger', 'Empfaenger.Id', 'Buchung.EmpfaengerId')
+        .leftJoin('Kategorie', 'Kategorie.Id', 'Buchung.KategorieId')
+
         .where(where)
+
         .then(fetchRows(res))
         .catch(catchError(res, next))
-
-    /*
-     var x = knex
-     .select()
-     //.select('Memo','Betrag')
-     .table('Konto')
-     .innerJoin('Buchung', 'Konto.Id', 'Buchung.KontoId')
-     .innerJoin('Empfaenger', 'Empfaenger.Id', 'Buchung.EmpfaengerId')
-     .where(where)
-
-
-     .toString();
-
-     console.log(x);
-     .then(fetchRows(res))
-     .catch(catchError(res, next))
-     */
   }
 }
 
@@ -61,7 +64,6 @@ var fetchRows = function (res, trans) {
     if (trans)
       rows = trans(rows);
     if (rows) {
-      //res.end(rows);
       res.end(JSON.stringify(rows));
     }
   }
