733711584cd506dbb376c26b2d5f7cd1acac30b4
detti <detlefziehlke@googlemail.com>
2015-03-17 10:05:56 +0100

    api changed: getEntriesById/id getEntriesByAccount/account.name|account.id getEntriesAll
    Test counterentry

--- test.data/test.finance.api.data.js
+++ test.data/test.finance.api.data.js
@@ -1,21 +1,23 @@
 "use strict";
 
 exports.getBalance_testResponse = [
-  {"konto": "AXA", "saldo": 50.82},
-  {"konto": "Anlagen", "saldo": 42985.51},
-  {"konto": "Girokonto", "saldo": 12978.57},
-  {"konto": "Kasse", "saldo": 186.77},
-  {"konto": "Kasse1", "saldo": 6},
-  {"konto": "Mutti", "saldo": -3},
-  {"konto": "Tagegeld", "saldo": 91142.48},
-  {"konto": "VISA", "saldo": -12.9}
+    {"konto": "AXA", "saldo": 50.82},
+    {"konto": "Anlagen", "saldo": 42985.51},
+    {"konto": "Girokonto", "saldo": 12978.57},
+    {"konto": "Kasse", "saldo": 186.77},
+    {"konto": "Kasse1", "saldo": 6},
+    {"konto": "Mutti", "saldo": -3},
+    {"konto": "Tagegeld", "saldo": 91142.48},
+    {"konto": "VISA", "saldo": -12.9}
 ];
 
-exports.getEntriesByAccount = {
-  ATUAccId: 2, ATUEntryCount: 5,
-  KasseAccName: 'Kasse', KasseEntryCount: 520
+exports.getEntries = {
+    ATUAccId: 2, ATUEntryCount: 5,
+    KasseAccName: 'Kasse', KasseEntryCount: 520,
+    Id: 12, Betrag: -4.9,
+    Id2: 22, CounterId: 24
 };
 
 exports.getAllEntries = {
-  NumberOfEntries: 873
+    NumberOfEntries: 873
 };
